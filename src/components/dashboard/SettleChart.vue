790de960c4b2d141b4c5567bec365dcc76a77b91
vijay <vijay@ardhika.com>
2024-03-31 23:57:30 +0530

    fix usericon in settlement chart

--- src/components/dashboard/SettleChart.vue
+++ src/components/dashboard/SettleChart.vue
@@ -5,23 +5,28 @@
         </v-card-title>
         <v-card-text class="pa-0">
             <v-card v-for="(i, idx) in data" :key="idx" class="elevation-10">
-                <v-card-text class="d-flex justify-space-between align-center">
-                    <v-row class="align-center">
-                        <v-col class="d-flex align-center" cols="5">
-                            <UserIcon :user="i.give" class="mr-4" size="18" style="width: 40px; height: 40px;" />
-                            <div class="d-flex flex-column">
-                                <h4>{{ i.give.displayName }}</h4>
+                <v-card-text class="d-flex">
+                    <v-row class="align-center justify-center">
+                        <div class="mr-2 d-inline-block">
+                            <UserIcon :user="i.give" size="18" />
+                        </div>
+                        <v-col class="d-flex align-center pa-1" cols="3">
+                            <div class="d-flex flex-column" style="overflow-x: hidden;">
+                                <h4 class="d-inline-block text-truncate">{{ i.give.displayName }}</h4>
                                 <h3 class="font-weight-bold success--text text-success" color="success">{{
                 Number(i.net).toFixed(2) }}</h3>
                             </div>
                         </v-col>
-                        <v-col cols="2">
+                        <v-col cols="2" class="pa-0">
                             <v-icon large size="40" color="teal">mdi-arrow-right-thin</v-icon>
                         </v-col>
-                        <v-col class="d-flex justify-space-between align-center" cols="5">
-                            <h4>{{ i.take.displayName }}</h4>
-                            <UserIcon :user="i.take" class="mr-2" size="18" style="width: 40px; height: 40px" />
+                        <v-col class="d-flex justify-space-between align-center pa-0" style="overflow-x: hidden;"
+                            cols="3">
+                            <h4 class="d-inline-block text-truncate">vijayaraagavan sadayappan</h4>
                         </v-col>
+                        <div class="ml-2 d-inline-block">
+                            <UserIcon :user="i.take" size="18" />
+                        </div>
                     </v-row>
                 </v-card-text>
                 <v-divider></v-divider>
